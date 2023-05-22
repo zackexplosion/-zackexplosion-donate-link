8edb8f89266a7b3a0e7005d2e9b70ca454346916
Zack <zack@zackexplosion.fun>
2023-05-22 20:36:32 +0800

    Init commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const { ObjectId, MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DB_URI
+const HOMEPAGE = process.env.HOMEPAGE || 'https://www.facebook.com/zackexplosion'
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4001
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+})
+
+app.set('view engine', 'pug');
+
+app.get('/', async (req, res) => {
+  res.redirect(HOMEPAGE)
+})
+
+app.get('/r/:id', async (req, res) => {
+
+  try {
+    const database = client.db('Links');
+    const links = database.collection("Links");
+    var link = await links.findOne({_id: new ObjectId(req.params.id)})
+
+    if(!link) return res.send('404')
+
+    if(link.redirectOnly && link.redirectOnly === true) {
+
+      // Update counter
+      links.updateOne(
+        {_id: new ObjectId(req.params.id)},
+        { $inc: { redirectTimes: 1 } }
+      )
+      return res.redirect(link.originLink)
+    }
+
+    return res.render('donate', { link });
+  } catch (error) {
+    console.error(error)
+    res.send('error')
+  }
+})
+
+
+async function start() {
+  try {
+    console.log('Connecting to DB')
+    await client.connect();
+
+    // start express server
+    console.log('Starting express server')
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`)
+    })
+
+  } catch(e) {
+    console.error(e)
+  }
+  finally {
+    
+  }
+}
+start()
